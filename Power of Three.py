0c4ea0a81939899e1d39cb05316890b992daa217
hsinhuibiga <hsinhuibiga@gmail.com>
2020-08-18 11:49:18 +1000

    Add files via upload

---
+++ Power of Three.py
@@ -0,0 +1,13 @@
+#Power of Three
+
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0: return False
+        if n == 1: return True
+        if n % 3 == 0:
+            return self.isPowerOfThree(n / 3)
+        return False
